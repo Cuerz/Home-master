7a32785ab44b765ed889db986739be7c0fee4edc
Cuerz <84277976+Cuerz@users.noreply.github.com>
2022-05-23 13:11:34 +0800

    Add files via upload

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  presets: [
+    '@vue/cli-plugin-babel/preset'
+  ]
+},{
+  "presets": [["es2015", { "modules": false }]],
+  "plugins": [
+    [
+      "component",
+      {
+        "libraryName": "element-ui",
+        "styleLibraryName": "theme-chalk"
+      }
+    ]
+  ]
+}
