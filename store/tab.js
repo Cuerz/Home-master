6e65ad953b35a8f1e399428b5aeb3d95d4e4ae5a
Cuerz <84277976+Cuerz@users.noreply.github.com>
2022-06-05 22:04:38 +0800

    Vuex

---
+++ store/tab.js
@@ -0,0 +1,80 @@
+import Cookies from "js-cookie"
+
+// import router from "../router"
+
+
+
+export default{
+    state:{
+        isCollapse:false,
+        tabsList:[
+            {
+                path:'/',
+                name:"home",
+                label:'首页',
+                icon:'home'
+            }
+        ],
+        currentMenu:null,
+        menu:[]
+    },
+    mutations:{
+        collapseMenu(state){
+            state.isCollapse=!state.isCollapse
+        },
+        selectMenu(state,val){
+            // 判断点击的值是否为首页，不是就获取值跳转
+            if(val.name !== 'home'){
+                state.currentMenu=val
+               const result= state.tabsList.findIndex(item=>item.name === val.name)
+               if(result === -1){
+                    state.tabsList.push(val)
+               }
+            }else{
+                state.currentMenu =null
+            }
+        },
+        closeTag(state,val){
+            const result=state.tabsList.findIndex(item=>item.name===val.name)
+            state.tabsList.splice(result,1)
+        },
+        setMenu(state,val){
+            state.menu=val
+            Cookies.set('menu',JSON.stringify(val))
+
+        },
+        clearMenu(state){
+            state.menu=[]
+            Cookies.remove('menu')
+        },
+        addMenu(state,router){
+            if(!Cookies.get('menu')){
+                return
+            }
+            const menu=JSON.parse(Cookies.get('menu'))
+            state.menu=menu
+            const menuArray=[]
+            menu.forEach(item=>{
+                if(item.children){
+                    item.children= item.children.map(item=>{
+                        item.component =()=>import(`../views/${item.url}`)
+                        return item
+                    })
+                    menuArray.push(...item.children)
+                }else{
+                    item.component =()=>import(`../views/${item.url}`)
+                    menuArray.push(item)
+                }
+            });
+            //路由的动态添加
+           
+            menuArray.forEach(item=>{
+                
+                router.addRoute('Main',item)
+               
+            })
+
+        }
+
+    }
+}
